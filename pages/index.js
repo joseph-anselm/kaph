4c6823c99bafe2752d68c6620370e5e5ceb7a580
Joseph Anselm <iamjoeanselm@gmail.com>
2022-04-26 06:53:24 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="  object-cover w-full bg-cover h-screen grid justify-center content-center bg-gradient-to-tl from-blue-700 to-black">
+    <div className="  object-cover w-full bg-cover h-screen grid justify-center content-center bg-gradient-to-t from-gray-700 to-gray-700">
       <img
         src="img/bg-4.jpg"
         className=" absolute h-full w-full object-cover mix-blend-overlay"
@@ -26,6 +26,17 @@ export default function Home() {
           </h1>
 
           <p className={styles.description}>Page is Under Construction </p>
+
+          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg
+              class="fill-current w-4 h-4 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+            <span>Contact us </span>
+          </button>
         </main>
 
         <footer className={styles.footer}>
