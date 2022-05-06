3c7e41f3414189837fa7cba82ec49f1e0b15c699
Joseph Anselm <iamjoeanselm@gmail.com>
2022-05-06 17:58:47 +0100

    Contact us fix commit

--- components/footer.js
+++ components/footer.js
@@ -107,7 +107,7 @@ export default function Footer() {
           <span className=" text-blue-200">
             {" "}
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://kaphglobal.com"
               target="_blank"
               rel="noopener noreferrer"
               className=" text-yellow-400 no-underline"
